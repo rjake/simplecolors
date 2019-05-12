b19fc619efc3facec80752ebdc277341b93eedc5
rjake <rjake@sas.upenn.edu>
2019-05-12 03:05:21 -0400

    Updates to color_table

--- R/globals.R
+++ R/globals.R
@@ -1,7 +1,11 @@
 if (getRversion() >= "3.0.0") {
   utils::globalVariables(
     c(
-      "color_table" # from data-color-table
+      "color_table", # from data-color-table
+      "H1", "H255", "H360", "L1", "S", "S1",
+      "color", "color_name", "color_sat",
+      "l_ord", "letter", "light",
+      "s_ord", "sat", "use_l", "use_s"
     )
   )
 }
