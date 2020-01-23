ad873a714d58dd1dfb121617201c3e86fcd63646
rjake <rjake@sas.upenn.edu>
2020-01-22 20:43:19 -0500

    Add spelling and fix typos

--- R/simplecolors.R
+++ R/simplecolors.R
@@ -7,7 +7,7 @@
 #' The sc() function is the main way to access color names. Colors can be called by including them as comma separated string values. For example: sc("brightblue4", "mutedred2")
 #'
 #'@section Color palettes:
-#' There are several functions that are prefixed with "sc_". These generate palettes of colors and can return hexcodes (default), a table, or a plot showing the colors selected.
+#' There are several functions that are prefixed with "sc_". These generate palettes of colors and can return hex codes (default), a table, or a plot showing the colors selected.
 #'
 #' @docType package
 #' @name simplecolors
