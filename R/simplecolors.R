d2d59a15b877fd223767c2f62b29ff708cd4be14
Jake <rjake@sas.upenn.edu>
2020-01-22 21:02:58 -0500

    Prep for cran (#35)
    * Minimal travis req
    
    * Update DESCRIPTION
    
    * Rename and edit package documentation
    
    * Delete simplecolors-package.Rd
    
    * Add OS list
    
    * Update NEWS.md
    
    * Add cran comments
    
    * Update .travis.yml
    
    * Update cran-comments.md
    
    * Fix typos
    
    * Add spelling and fix typos

---
+++ R/simplecolors.R
@@ -0,0 +1,14 @@
+#' simplecolors: A package for accessing color names using a standardized nomenclature
+#'
+#' The simplecolors package provides two categories of functions:
+#' color names and color palettes
+#'
+#' @section Color names:
+#' The sc() function is the main way to access color names. Colors can be called by including them as comma separated string values. For example: sc("brightblue4", "mutedred2")
+#'
+#'@section Color palettes:
+#' There are several functions that are prefixed with "sc_". These generate palettes of colors and can return hex codes (default), a table, or a plot showing the colors selected.
+#'
+#' @docType package
+#' @name simplecolors
+NULL
