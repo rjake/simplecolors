799ff3bd82c9d9c3266ff6bf0d090093d8e9d1b4
Jake <rjake@sas.upenn.edu>
2022-06-05 23:08:03 -0400

    Suppress warning in test

--- tests/testthat/test-build_colors.R
+++ tests/testthat/test-build_colors.R
@@ -1,6 +1,6 @@
 context("test-build_colors")
 
 test_that("build_colors returns table", {
-  x <- build_colors()
+  x <- build_colors() %>% suppressWarnings()
   expect_equal(dim(x), c(176, 15))
 })
